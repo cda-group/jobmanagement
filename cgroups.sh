3bf6e6cd0dd744446883f89bedcec5ccdbf96539
Max Meldrum <max@meldrum.se>
2018-08-21 16:38:04 +0200

    Fix cgroups script

--- cgroups.sh
+++ cgroups.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # TaskManager
-mkdir -p /sys/fs/cgroup/cpu/taskmanager
-mkdir -p /sys/fs/cgroup/memory/taskmanager
+sudo mkdir -p /sys/fs/cgroup/cpu/taskmanager
+sudo mkdir -p /sys/fs/cgroup/memory/taskmanager
 
 # Containers
-mkdir -p /sys/fs/cgroup/cpu/containers
-mkdir -p /sys/fs/cgroup/memory/containers
+sudo mkdir -p /sys/fs/cgroup/cpu/containers
+sudo mkdir -p /sys/fs/cgroup/memory/containers
 
 
 # Set User rights to the directories
 
-chown -R ${USER}: /sys/fs/cgroup/cpu/taskmanager
-chown -R ${USER}: /sys/fs/cgroup/cpu/containers
+sudo chown -R ${USER}: /sys/fs/cgroup/cpu/taskmanager
+sudo chown -R ${USER}: /sys/fs/cgroup/cpu/containers
 
-chown -R ${USER}: /sys/fs/cgroup/memory/taskmanager
-chown -R ${USER}: /sys/fs/cgroup/memory/containers
+sudo chown -R ${USER}: /sys/fs/cgroup/memory/taskmanager
+sudo chown -R ${USER}: /sys/fs/cgroup/memory/containers

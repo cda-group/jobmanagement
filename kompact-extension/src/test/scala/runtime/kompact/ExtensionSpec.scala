55133dc4304a92dc25420ef31d0ed258b8239808
Max Meldrum <max@meldrum.se>
2018-09-16 22:15:12 +0200

    Removed kompact-ext sample files and added tests

--- kompact-extension/src/test/scala/runtime/kompact/ExtensionSpec.scala
+++ kompact-extension/src/test/scala/runtime/kompact/ExtensionSpec.scala
@@ -3,6 +3,9 @@ package runtime.kompact
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
+import runtime.kompact.ExtensionActor.TestAsk
+import runtime.kompact.KompactAsk.AskSuccess
+import runtime.kompact.messages.{Hello, KompactAkkaEnvelope, KompactAkkaMsg}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,7 +17,7 @@ object ExtensionSpec {
       | akka.loglevel = "OFF"
       | akka.extensions = ["runtime.kompact.KompactExtension"]
       | akka.kompact.port = 1337
-      | akka.kompact.host = "localhost"
+      | akka.kompact.host = "127.0.0.1"
       | akka.log-dead-letters-during-shutdown = false
       | akka.log-dead-letters = 0
     """.stripMargin))
@@ -33,25 +36,22 @@ class ExtensionSpec extends TestKit(ExtensionSpec.actorSystem)
   "KompactExtension" must {
 
     "Register executor, perform ask request and handle DeathWatch" in {
-      // TODO: TestClient must be rewired
-
-      /*
       val extensionActor = system.actorOf(ExtensionActor(self), extensionActorName)
+      val formattedPath = extensionActor.path.toSerializationFormatWithAddress(ExternalAddress(system).addressForAkka)
       val port = system.settings.config.getInt("akka.kompact.port")
       val host = system.settings.config.getString("akka.kompact.host")
       val simpleClient = new SimpleClient()
-      Future(simpleClient.run(host, port))
+      Future(simpleClient.run(host, port, formattedPath))
       // We should receive an ExecutorUp msg when a client connects
       expectMsgType[ExecutorUp]
 
-      import ExtensionActor._
       val msg = KompactAkkaMsg().withHello(Hello("ask"))
       extensionActor ! TestAsk(msg)
-      expectMsgType[AskSuccess]
+      expectMsgType[KompactAkkaMsg]
 
+      import ExtensionActor._
       extensionActor ! TestDeathWatch
       expectMsgType[ExecutorTerminated]
-      */
     }
   }
 
